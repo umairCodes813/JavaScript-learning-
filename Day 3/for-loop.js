87e3a54e913b219b8c022ace7823aab3014b3beb
umairCodes813 <umair66h@gmail.com>
2026-02-26 16:45:22 +0500

    Now i perform some difficult questions of for loop

--- Day 3/for-loop.js
+++ Day 3/for-loop.js
@@ -2,39 +2,66 @@
 
 // Print the numbers from 1 to 10.
 
-for ( let i = 1 ; i <= 10 ; i++ ) {
-    console.log( i ) ; 
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
 }
 
 // Print reverse counting from 10 to 1.
 
-for (let i = 10; i >= 1 ; i--) {
-    console.log( i ) ;
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
 }
 
 // Print only even numbers from 1 to 20.
 
-for (let i = 2; i <= 20 ; i += 2 ) {
-    if ( i % 2 == 0 ) {
-        console.log( i ) ;
+for (let i = 2; i <= 20; i += 2) {
+    if (i % 2 == 0) {
+        console.log(i);
     }
 }
 
 //  Print only the odd numbers from 1 to 20.
 
-for (let i = 1; i <+ 20 ; i++) {
-    if ( i % 2 != 0 ) {
-        console.log( i )
+for (let i = 1; i < + 20; i++) {
+    if (i % 2 != 0) {
+        console.log(i)
     }
 }
 
-// Print the table.
+// Take a number from the user and print its table.
 
-let n = prompt( " Enter any number whose table you want " )
+let n = prompt(" Enter any number whose table you want ")
 n = Number.parseInt(n)
-let val = prompt( " Enter the value till the table you want to print " )
+let val = prompt(" Enter the value till the table you want to print ")
 val = Number.parseInt(val)
-for( let i = 1 ; i <= val ; i++ ){
-    console.log( n + " x " + i + " = " + (n * i) )
+for (let i = 1; i <= val; i++) {
+    console.log(n + " x " + i + " = " + (n * i))
 }
 
+
+// Sum all the numbers from 1 to 50.
+
+let sum = 0
+for (let i = 0; i <= 50; i++) {
+    sum += i
+    // console.log( i + " + " )
+}
+console.log( " Sum of first 50 natural numbers is " + sum )
+
+
+// Take a number from the effect and find the sum from 1 to that number
+
+let num = prompt( " Enter the number sum you want " )
+num = Number.parseInt(num)
+let add = 0
+for (let i = 1; i <= num; i++) {
+    add += i
+    // console.log( i + " + " )
+}
+console.log( " Sum from 1 to " + num + " is " + add )
+
+// Print the squares of the numbers 1 to 10.
+
+ for( let i = 1 ; i <= 10 ; i++  ){
+    console.log( " Square of "+ i  + " is " + ( i * i ) )
+ }
