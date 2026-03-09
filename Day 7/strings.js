8b526ddc9a640ec8c0ae170fb3482a1dd5f8643c
umairCodes813 <umair66h@gmail.com>
2026-03-09 22:36:54 +0500

    Today i study strings and Template Literals and escape sequence and practice them just shortly

--- Day 7/strings.js
+++ Day 7/strings.js
@@ -1,2 +1,26 @@
 // Today we start practicing strings and its methods
+let s = "He said \"Javascript is amazing\" "
+console.log(s)
 
+let a = "umair"
+console.log(a)
+let b = 'Ahmad'
+console.log(b)
+
+let name = "Ali"
+let ans = `My name is ${name}`
+console.log(ans)
+let age = 20
+let answer = `${name} is ${age} years old.`
+console.log(answer)
+
+let x = "Hello\nWorld!"
+console.log(x)
+
+let y = "umairahmad"
+console.log(y.toUpperCase())
+
+let sentence = "I love coding"
+
+console.log(sentence.length)
+console.log(sentence.toUpperCase())
